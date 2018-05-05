6905957b0eb8761099df39aa78b4b464c71d9715
progresivoJS <progresivoJS@gmail.com>
2018-05-05 18:30:13 +0900

    feat: implement continue_crawl function.

--- http-request.py
+++ http-request.py
@@ -7,3 +7,19 @@ html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 print(soup.find(id="firstHeading"))
+
+def continue_crawl(search_history, target_url):
+    """
+    Determine whether we continue crwal.
+
+    search_history : a list of strings which are the urls of Wikipedia articles
+    target_url : a url which we want to find
+    """
+    if search_history[-1] == target_url:
+        return False
+    elif len(search_history) > 25:
+        return False
+    elif search_history[-1] in search_history[:-1]:
+        return False
+    else:
+        return True

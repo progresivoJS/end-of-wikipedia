10d5adabb231fe0a15298a0b8adf7d1bc5501b6c
progresivoJS <progresivoJS@gmail.com>
2018-05-05 18:41:46 +0900

    refactor: remove unused code

--- WikipediaCrawl.py
+++ WikipediaCrawl.py
@@ -1,13 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
-
-response = requests.get("https://en.wikipedia.org/wiki/Dead_Parrot_sketch")
-
-html = response.text
-
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.find(id="firstHeading"))
-
 def continue_crawl(search_history, target_url, max_steps=25):
     """
     Determine whether we continue crwal.

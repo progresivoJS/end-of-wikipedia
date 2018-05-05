dcb15a73f36ea4d1c4c52ae1387ec47e72890db8
progresivoJS <progresivoJS@gmail.com>
2018-05-05 17:32:56 +0900

    feat: fetch response from url and extract specific tag from html using requests, beautifulsoup4.

---
+++ http-request.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://en.wikipedia.org/wiki/Dead_Parrot_sketch")
+
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.find(id="firstHeading"))
